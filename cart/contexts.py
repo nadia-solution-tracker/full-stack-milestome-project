b8408669531a23731cfe3d28752e9c407b61f616
Ubuntu <ubuntu@ip-172-31-38-10.ec2.internal>
2019-10-03 19:08:01 +0000

    Updated cart views and context to allow Out of stock message if the product instock value is 0

--- cart/contexts.py
+++ cart/contexts.py
@@ -12,19 +12,22 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
-
-    
+    updated_stock = 0
     for id, quantity in cart.items():
         product = get_object_or_404(Product, pk=id)
+        updated_stock = product.instock
         total += quantity * product.price
         product_count += quantity
         cart_items.append({'id': id, 
                             'quantity': quantity,    
                             'product': product,
+                            'updated_stock': updated_stock
                             
         })
     
     return {'cart_items': cart_items, 
             'total': total, 
             'product_count': product_count,
-           }
+            'updated_stock': updated_stock   
+        
+    }
--- cart/views.py
+++ cart/views.py
@@ -9,22 +9,34 @@ def view_cart(request):
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity = int(request.POST.get('quantity'))
+    product = get_object_or_404(Product, pk=id)
     cart = request.session.get('cart', {})
-    
+    updated_stock = product.instock
     if id in cart:
         cart[id] = int(cart[id]) + quantity      
     else:
         cart[id] = cart.get(id, quantity) 
     request.session['cart'] = cart
+    for k, v in cart.items():
+        if k == id:
+            updated_stock -= v
+            if updated_stock == 0 and id in cart:
+                update = Product.objects.filter(id = id).update(instock = updated_stock)
     return redirect(reverse('products'))
     
 def adjust_cart(request, id):
     """Adjust the quantity of the specified product to the specified amount"""
     quantity = int(request.POST.get('quantity'))
+    product = get_object_or_404(Product, pk=id)
     cart = request.session.get('cart',{})
-    
+    updated_stock = product.instock
     if quantity > 0:
         cart[id] = quantity
+        for k,v in cart.items():
+            if k == id:
+                updated_stock -= v
+                if updated_stock == 0:
+                    update = Product.objects.filter(id = id).update(instock = updated_stock)
     else:
         cart.pop(id)
 
@@ -32,7 +44,10 @@ def adjust_cart(request, id):
     return redirect(reverse('view_cart'))
     
 def remove_from_cart(request, id):
+    product = get_object_or_404(Product, pk=id)
     cart = request.session.get('cart', {})
+    updated_stock = product.instock
+    update = Product.objects.filter(id = id).update(instock = updated_stock)
     cart.pop(id)
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
